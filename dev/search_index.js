39e5130a32f6951ea58ee394b40748da080a2490
Documenter.jl <documenter@juliadocs.github.io>
2024-12-05 16:20:11 +0000

    build based on cd5ed0a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CollisionDistance","category":"page"},{"location":"#CollisionDistance","page":"Home","title":"CollisionDistance","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CollisionDistance.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CollisionDistance]","category":"page"}]
+}
